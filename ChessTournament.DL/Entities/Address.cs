3e4a5f3ccd53776fb03a9f381f9c9ea68dda01e9
Michael Arabia <arabia.michael@gmail.com>
2022-08-08 15:47:38 +0200

    Add Entities : Address & Country Modify Entities : Tournament

---
+++ ChessTournament.DL/Entities/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTournament.DL.Entities
+{
+    public class Address
+    {
+        public Country Country;
+        public string PostalCode;
+        public string Street;
+        public string HouseNumber;
+        public string City;
+        public string Province;
+    }
+}
