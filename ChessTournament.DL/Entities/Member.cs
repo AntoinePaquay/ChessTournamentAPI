d56637f41d619b91eedfdbf64cebf9ee664f5a2e
Antoine <tempmailpourpres@gmail.com>
2022-08-08 16:28:19 +0200

    Matchup entity, Result enum

--- ChessTournament.DL/Entities/Member.cs
+++ ChessTournament.DL/Entities/Member.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessTournament.DL.Enumerations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,13 @@ namespace ChessTournament.DL.Entities
     public class Member
     {
         public Guid Id { get; set; }
-        public string Pseudo { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Pseudo { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
         public DateTime Birthday { get; set; }
-
+        public double ELO { get; set; }
+        public Role Role { get; set; }
+        public Gender Gender { get; set; }
+        public virtual ICollection<Tournament> Tournaments { get; set; } = new List<Tournament>();
     }
 }
