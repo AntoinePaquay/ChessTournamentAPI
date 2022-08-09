45347a0d447729331452c5964ad690d3347612e0
Michael Arabia <arabia.michael@gmail.com>
2022-08-09 14:36:48 +0200

    Creation Interfaces (IMatchUpRepository & IMemberRepository) Creation Repositories (RepositoryMatchUp & RepositoryMember)

---
+++ ChessTournament.DAL/Interfaces/IMatchupRepository.cs
@@ -0,0 +1,14 @@
+﻿using ChessTournament.DL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTournament.DAL.Interfaces
+{
+    public interface IMatchupRepository : IRepository<Guid, Matchup>
+    {
+
+    }
+}
