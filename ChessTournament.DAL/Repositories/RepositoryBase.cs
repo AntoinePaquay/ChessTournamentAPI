a866262cd2bc1515661d2a1d0739f2c3b8346602
Michael Arabia <arabia.michael@gmail.com>
2022-08-09 11:10:01 +0200

    Add ChessTournamentContext

--- ChessTournament.DAL/Repositories/RepositoryBase.cs
+++ ChessTournament.DAL/Repositories/RepositoryBase.cs
@@ -1,4 +1,5 @@
-﻿using ChessTournament.DAL.Interfaces;
+﻿using ChessTournament.DAL.Context;
+using ChessTournament.DAL.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,13 @@ namespace ChessTournament.DAL.Repositories
     public class RepositoryBase<TKey, TEntity> : IRepository<TKey, TEntity>
         where TEntity : IEntity<TKey>
     {
+        protected ChessTournamentContext _context;
+
+        public RepositoryBase(ChessTournamentContext context)
+        {
+            _context = context;
+        }
+
         public TKey Create(TEntity entity)
         {
             throw new NotImplementedException();
