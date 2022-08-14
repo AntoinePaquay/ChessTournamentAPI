b1daaa79171ce7908ab3c35ccf4b63581bd508fe
Antoine Paquay <antoinepaquay64@gmail.com>
2022-08-15 01:02:41 +0200

    Added Matchup controller, service and iservice

---
+++ ChessTournament.API/Controllers/MatchupController.cs
@@ -0,0 +1,23 @@
+﻿using ChessTournament.BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessTournament.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchupController : ControllerBase
+    {
+        private IMatchupService _MatchupService;
+        public MatchupController(IMatchupService matchupService)
+        {
+            _MatchupService = matchupService;
+        }
+
+        [HttpPatch]
+        public IActionResult SetResult(Guid matchupId, int result)
+        {
+
+        }
+    }
+}
---
+++ ChessTournament.BLL/Services/MatchupService.cs
@@ -0,0 +1,13 @@
+﻿using ChessTournament.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTournament.BLL.Services
+{
+    public class MatchupService : IMatchupService
+    {
+    }
+}
